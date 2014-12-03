012a40225aced418eb7b777e17b62e27e838dbd1
gimu <mail@gimu.org>
2014-12-03 19:30:04 +0100

    Add config.php

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Base URL
+define('URL', 'http://localhost/unio-php/');
+
+/**
+ * Database settings
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'unio-php');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+/**
+ * Miscellaneous settings
+ */
+define('DEBUG_MODE', true);

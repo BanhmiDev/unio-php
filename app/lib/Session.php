00408f81427f626acce6c1a6abb36892f6a81562
gimu <mail@gimu.org>
2014-12-05 19:13:30 +0100

    Fix session class and change to static

--- app/lib/Session.php
+++ app/lib/Session.php
@@ -2,26 +2,32 @@
 
 /**
  * Session class
+ * Example static call: Session:init();
  */
 class Session {
     function __construct() {
-        // PHP >= 5.4.0 PHP_SESSION_NONE
-        if (session_status() == '') {
+
+    }
+
+    public static function init() {
+        // PHP >= 5.4.0 
+        // session_status() == PHP_SESSION_NONE
+        if (session_id() == '') {
             session_start();
         }
     }
 
-    public function set($key, $value) {
+    public static function set($key, $value) {
         $_SESSION[$key] = $value;
     }
 
-    public function get($key) {
+    public static function get($key) {
         if (isset($_SESSION[$key])) {
             return $_SESSION[$key];
         }
     }
 
-    public function destroy() {
+    public static function destroy() {
         session_destroy();
     }
 }

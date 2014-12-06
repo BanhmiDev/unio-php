5787c29bde90a1b817ea0c283faa46f8b2e8a2cb
gimu <mail@gimu.org>
2014-12-06 18:59:55 +0100

    Add settings class

---
+++ app/lib/Settings.php
@@ -0,0 +1,26 @@
+<?php
+
+class Settings {
+
+    protected static $settings;
+
+    public static function init() {
+        $db = new Database();
+        $_settings = array();
+
+        // Read settings from database
+        $db_settings = $db->query("SELECT name, settings FROM unio_settings");
+        foreach ($db_settings as $db_setting) {
+            if (!isset($_settings)) $_settings = array($db_setting['name'] => $db_setting['settings']);
+            else $_settings[$db_setting['name']] = $db_setting['settings'];
+        }
+
+        self::$settings = $_settings;
+    }
+
+    public static function get($key) {
+        if (isset(self::$settings[$key])) {
+            return self::$settings[$key];
+        }
+    }
+}
